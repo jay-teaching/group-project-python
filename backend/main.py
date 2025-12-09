21ee5a7293831516c5dc21fc60634b8dea3c0e48
KerenyiKrisz <91314846+KerenyiKrisz@users.noreply.github.com>
2025-12-09 21:16:30 +0000

    please work

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from prediction import make_prediction
 
